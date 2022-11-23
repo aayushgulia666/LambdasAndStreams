40c58b3c8fc017ae2664a45d4864cfda08d69d41
Aayush Gulia <aayushgulia07@outlook.com>
2022-11-23 21:24:08 +0530

    Print hello world using lambda.

--- src/main/java/com/aayushgulia/Main.java
+++ src/main/java/com/aayushgulia/Main.java
@@ -2,6 +2,18 @@ package com.aayushgulia;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Greeting greeting = new HelloGreeting();
+        greeting.sayHello();
+
+        Greeting greeting1 = new Greeting() {
+            @Override
+            public void sayHello() {
+                System.out.println("Hello World again!");
+            }
+        };
+        greeting1.sayHello();
+
+        Greeting greeting2 = () -> System.out.println("Hello world using lambda!");
+        greeting2.sayHello();
     }
 }
